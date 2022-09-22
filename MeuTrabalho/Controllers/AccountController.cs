0290edf4e2397877303f614a20c11f5bd4b92c94
Fabricio Catae <fcatae@hotmail.com>
2022-09-22 14:42:43 -0300

    update connection strings

--- MeuTrabalho/Controllers/AccountController.cs
+++ MeuTrabalho/Controllers/AccountController.cs
@@ -23,7 +23,7 @@ namespace MeuTrabalho.Controllers
         {
             try
             {
-                SqlConnection connection = new SqlConnection("Server=martedb.database.windows.net;Database=db2022;User=aclogin;Password=homework-mar22");
+                SqlConnection connection = new SqlConnection("Server=martesql.database.windows.net;Database=db2022;User=aclogin;Password=homework-set22");
                 SqlCommand cmd = new SqlCommand($"SELECT username FROM tbLogin WHERE email='" + model.Email + "' AND pwd='" + model.Password + "'", connection);
 
                 connection.Open();
